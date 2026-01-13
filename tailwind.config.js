41d1c30e769c1dcacba62d9a725b99bf128524cb
hunde51 <hundetesfa6@gmail.com>
2026-01-13 19:50:55 +0300

    add Maven POM file and Tailwind CSS configuration for project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./src/main/webapp/jsp/**/*.jsp",
+        "./src/main/webapp/static/js/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
